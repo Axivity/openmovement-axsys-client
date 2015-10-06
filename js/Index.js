0ad10fed233065e5769d1613546ced6b92302a9b
opensourcegeek_axivity <praveen@axivity.com>
2015-10-06 17:24:49 +0100

    Added read/write mechanism

--- js/Index.js
+++ js/Index.js
@@ -8,6 +8,7 @@ require("babel/polyfill");
 import React, { Component } from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import moment from 'moment';
 
 /* internal imports*/
 import App from './containers/AxsysApp';
@@ -17,11 +18,20 @@ import axsysApp from './reducers/reducers';
 
 let store = createStore(axsysApp);
 
+const WRITTEN = Symbol();
+const WRITTEN_AND_READ = Symbol();
+
+const TIMEOUT_IN_SECONDS = 10;
+const CHECK_FREQUENCY_IN_SECONDS = 1;
+
+let connections = {};
+
 let api = new AXApi(
     onDeviceAdded(store),
     onDeviceRemoved(store),
     onConnected(store),
-    onDisconnected(store)
+    onDisconnected(store),
+    onDataReceived
 );
 
 console.log(store.getState());
@@ -47,13 +57,47 @@ function onDisconnected(store) {
     }
 }
 
+function onDataReceived(data) {
+    console.log('Data is in the app');
+    console.log(data);
+    connections[data.path] = WRITTEN_AND_READ;
+}
+
 function getHardwareAndSoftwareVersion(device) {
     let options = {};
-    options.path = device._id;
+    let path = device._id;
+    options.path = path;
     console.log(api);
+
     api.connect(options, (response) => {
         console.log(response);
+        if(response === null) { // void method
+            let writeOptions = {};
+            writeOptions.path = path;
+            writeOptions.command = 'ID\r\n';
+            api.write(writeOptions, (writeResponse) => {
+                console.log(writeResponse);
+                connections[path] = WRITTEN;
+            });
+
+            checkIfDataReceivedAndCloseConnection(path);
+        }
     });
+}
+
+function checkIfDataReceivedAndCloseConnection(path) {
+    let endTime = moment().add(TIMEOUT_IN_SECONDS, 'second');
+    var checker = setInterval(() => {
+        if (connections[path] === WRITTEN_AND_READ || moment() >= endTime) {
+            let options = {};
+            options.path = path;
+            api.disconnect(options, (response) => {
+                console.log('Closed connection');
+                console.log(response);
+            });
+            clearInterval(checker);
+        }
+    }, CHECK_FREQUENCY_IN_SECONDS);
 }
 
 function onConnected(store) {
--- js/ax-client.js
+++ js/ax-client.js
@@ -6,13 +6,15 @@ export default class AXApi {
     constructor(onDeviceAdded,
                 onDeviceRemoved,
                 onConnected,
-                onDisconnected) {
+                onDisconnected,
+                onDataReceived) {
 
         this.api = new AX.API(
             onDeviceAdded,
             onDeviceRemoved,
             onConnected,
-            onDisconnected);
+            onDisconnected,
+            onDataReceived);
     }
 
     getDevices(callback) {
@@ -25,6 +27,18 @@ export default class AXApi {
 
     connect(options, callback) {
         this.api.connect(options, (payload) => {
+            callback(payload);
+        });
+    }
+
+    write(options, callback) {
+        this.api.write(options, (payload) => {
+            callback(payload);
+        });
+    }
+
+    disconnect(options, callback) {
+        this.api.disconnect(options, (payload) => {
             callback(payload);
         });
     }
